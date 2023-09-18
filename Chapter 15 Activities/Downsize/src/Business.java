42c5e506e3937935fd50ad4f4c648b7f894f5dd4
Vinay Narahari <111399243+vinaynarahari@users.noreply.github.com>
2023-09-17 20:23:43 -0500

    did the downsize program

--- Chapter 15 Activities/Downsize/src/Business.java
+++ Chapter 15 Activities/Downsize/src/Business.java
@@ -1,4 +1,5 @@
 import java.util.LinkedList;
+import java.util.ListIterator;
 
 /**
  * Business utility methods.
@@ -13,6 +14,14 @@ public class Business
      */
     public static void downsize(LinkedList<String> employeeNames, int n)
     {
-        ...
+        int i =0;
+         ListIterator<String> iterator = employeeNames.listIterator();
+        while(iterator.hasNext()){
+            i++;
+            if(i == n ){
+                iterator.remove();
+                i =0;
+            }
+        }
     }
 }
