19e5e772ba50bcf29d68d591a41ce0e9b1ab09f1
Vinay Narahari <111399243+vinaynarahari@users.noreply.github.com>
2023-10-23 13:47:26 -0500

    changes to linkedlistqueue

--- Chapter 16 Activities/FirstToLast/src/LinkedListQueue.java
+++ Chapter 16 Activities/FirstToLast/src/LinkedListQueue.java
@@ -1,3 +1,5 @@
+import java.util.NoSuchElementException;
+
 /**
     Add a method firstToLast to this implementation of a queue.
     The method moves the element at the head of the queue to
@@ -23,7 +25,7 @@ public class LinkedListQueue
     */
     public void firstToLast()
     {
-        . . .
+        this.add(remove());
 
 
 
