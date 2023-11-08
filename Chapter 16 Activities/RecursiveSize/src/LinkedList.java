42a1539c47818880377c90cc1c72c4cea415c684
Vinay Narahari <111399243+vinaynarahari@users.noreply.github.com>
2023-11-07 22:05:33 -0600

    changes to recursive size

--- Chapter 16 Activities/RecursiveSize/src/LinkedList.java
+++ Chapter 16 Activities/RecursiveSize/src/LinkedList.java
@@ -26,9 +26,16 @@ public class LinkedList
     */
     public int size()
     {
-        . . .
+        return size(first);
     }
-
+    public static int size(Node start){
+        if(start == null){
+            return 0;
+        }
+        else{
+            return 1 + size(start.next);
+        }
+    }
 
     /**
         Returns the first element in the linked list.
